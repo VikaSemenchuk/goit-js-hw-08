5a237fc428ae9bfb3e285c96bfeb4ac2f99d716e
VikaSemenchuk <viktoriia.semenchuk@gmail.com>
2023-01-30 01:37:14 +0100

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import localStorageService from './localstorage';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const localStorageKey = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onSaveStorage, 1000));
+
+function onSaveStorage(event) {
+  localStorageService.save(localStorageKey, event.seconds);
+}
+
+setCurrentTime();
+
+function setCurrentTime() {
+  const saveTime = localStorageService.load(localStorageKey);
+  if (saveTime) {
+    player.setCurrentTime(saveTime);
+  }
+}
