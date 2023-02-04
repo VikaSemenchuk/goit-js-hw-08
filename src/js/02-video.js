06a967d030fb5e73068f82682cb3aa25285f3ae1
VikaSemenchuk <viktoriia.semenchuk@gmail.com>
2023-02-04 12:09:40 +0100

    add event.percent in task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,12 +6,18 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const localStorageKey = 'videoplayer-current-time';
 
+
+
 player.on('timeupdate', throttle(onSaveStorage, 1000));
 
 function onSaveStorage(event) {
   localStorageService.save(localStorageKey, event.seconds);
-  if (event.seconds === event.duration) {
+  // localStorage.setItem(localStorageKey, event.seconds)
+
+  if (event.seconds === event.duration) {// або event.percent === 1
+     
     localStorageService.remove(localStorageKey);
+    // localStorage.removeItem(localStorageKey)
   }
 }
 
